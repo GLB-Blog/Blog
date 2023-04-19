737b1c24aca8318cceb825016481d221d04042d7
Yaga987 <987142.yaga@gmail.com>
2023-04-19 17:31:12 +0300

    Write page and button
    Write page changed for request and write button has new func when a teacher login it is ative else it is not

--- api/routes/navbar.js
+++ api/routes/navbar.js
@@ -1,18 +1,23 @@
-// import express from "express";
+import express from "express";
 import {db} from "../db.js";
 
-// const router = express.Router();
+const router = express.Router();
 
-app.get("/navbar/:userId", async (req, res) => {
-    const { userId } = req.params;
-    const q = 'SELECT EXISTS(SELECT 1 FROM teachers WHERE userid = ?) AS userExists';
+router.get('/:userId', (req, res) => {
+  const userId = parseInt(req.params.userId);
+  const q = "SELECT EXISTS(SELECT 1 FROM teachers WHERE userid = ?) AS isTeacher";
+  db.query(q, userId, (err, data) => {
+    if (err) return res.status(500).json(err);
 
-    db.query(q, userId, (err, data) => {
-        if (err) return res.status(500).json(err);
-        const userExists = data[0].userExists;
-        if(userExists) return res.status(200).json(true);
-        else return res.status(200).json(false);
-    });
+    const isTeacher = !!data[0].isTeacher;
+    if (isTeacher) {
+      return res.status(200).json("Teacher");
+    } else if (data[0].isTeacher === 0) {
+      return res.status(200).json("Student");
+    } else {
+      return res.status(404).json("User not found");
+    }
   });
+});
 
 export default router;

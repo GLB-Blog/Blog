2a4a56203736bf077ff8380629a61d062bfbe514
CarpeDiem0110 <eEren_Ceylan@hotmail.com>
2023-04-21 22:04:57 +0300

    ....

--- api/db.js
+++ api/db.js
@@ -4,6 +4,6 @@ import mysql from "mysql"
 export const db = mysql.createConnection({
     host:"localhost",
     user: "root",
-    password : "123",
+    password: password,
     database: "glb_blog"
 })
